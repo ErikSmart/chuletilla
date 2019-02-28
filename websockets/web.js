d33eb07e7872bf4b9c4a763b004d802e5ee86de2
Erik <erikcat06@gmail.com>
2019-02-28 13:23:41 -0600

    Agregando llaves
    Solo por diversion

--- websockets/web.js
+++ websockets/web.js
@@ -5,7 +5,7 @@ boton.addEventListener('click', e => {
   var conexion = new XMLHttpRequest();
   conexion.open('GET', 'datos.html', true);
   conexion.addEventListener('load', e => {
-    respuesta.innerHTML = e.target.responseText;
+    respuesta.innerHTML = `${e.target.responseText}`;
   });
   conexion.send();
 
@@ -16,7 +16,7 @@ window.addEventListener('keyup', ini, false);
 function ini() {
 
   var caja_busqueda = document.getElementById('buscar');
-  res(caja_busqueda.value);
+  res(`${caja_busqueda.value}`);
 }
 var con;
 
@@ -31,7 +31,7 @@ function accion() {
   var r = document.getElementById('respuesta');
 
   if (con.readyState == 4) {
-    let dato = JSON.parse(con.responseText)
+    let dato = JSON.parse(`${con.responseText}`)
     r.innerHTML = `${dato.title}`;
 
   }
