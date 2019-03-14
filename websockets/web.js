c2da55bd4aed67a758b698390424527796ea7c81
Erik <erikcat06@gmail.com>
2019-03-14 13:50:58 -0600

    Agregando createDocumentFragment y usando createElement para crear los elementos

--- websockets/web.js
+++ websockets/web.js
@@ -3,13 +3,41 @@ var boton = document.getElementById('btn');
 
 boton.addEventListener('click', e => {
   var conexion = new XMLHttpRequest();
-  conexion.open('GET', 'datos.html', true);
+  conexion.open('GET', 'datos.json', true);
+  if (conexion.readyState == 1) {
+     respuesta.innerHTML=`Cargando...`;
+  }
   conexion.addEventListener('load', e => {
-    respuesta.innerHTML = `${e.target.responseText}`;
+    let datos = JSON.parse(e.target.responseText);
+    larespuesta(datos);
   });
   conexion.send();
-
 });
+
+
+var larespuesta = datos => {
+  respuesta.innerHTML='';
+  //uso de createDocumentFragment
+  var fragmento = document.createDocumentFragment();
+ for (var i = 0; i < datos.length; i++) {
+   let titulo = document.createElement('h1');
+   let contenido = document.createElement('p');
+   let ititulo = document.createTextNode(`${datos[i].titulo}`);
+   let icontenido = document.createTextNode(`${datos[i].contenido}`);
+   // respuesta.appendChild(titulo);
+   // respuesta.appendChild(contenido);
+   contenido.appendChild(icontenido);
+   titulo.appendChild(ititulo);
+   fragmento.appendChild(titulo);
+   fragmento.appendChild(contenido);
+ }
+respuesta.appendChild(fragmento);
+
+
+    // respuesta.innerHTML = `${datos[1].titulo}`;
+}
+
+
 // Viejo metodo (rescantando datos)
 window.addEventListener('keyup', ini, false);
 
@@ -23,6 +51,9 @@ var con;
 function res(valor) {
   con = new XMLHttpRequest();
   con.open('GET', 'https://jsonplaceholder.typicode.com/posts/' + valor, true);
+  if (con.readyState== 1) {
+  document.getElementById('respuesta').innerHTML='Cargando ...';
+  }
   con.onreadystatechange = accion;
   con.send();
 }
@@ -33,6 +64,7 @@ function accion() {
   if (con.readyState == 4) {
     let dato = JSON.parse(`${con.responseText}`)
     r.innerHTML = `${dato.title}`;
-
+    r.style.display='inline';
   }
+
 }
